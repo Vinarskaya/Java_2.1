59f7ec6abb18a94137b6fa56f28040bed13d350b
Vinarskaya <t-vinarskaya@mail.ru>
2022-07-14 21:06:18 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 20_000;
+        int oneMile = 20;
+        int bonus = ticketPrice / oneMile;
+        System.out.println("Количество начисленных миль: " + bonus);
+
+    }
+}
